5dbbde1e4d8708c6938eba2c9be7c59e4b4507e9
eala-lah <eala-lah@student.hive.fi>
2024-11-01 16:26:49 +0200

    added a freeing functionality

--- src/main.c
+++ src/main.c
@@ -6,19 +6,19 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:56:06 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/11/01 15:14:03 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/11/01 15:55:22 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	launch_frc(mlx_t *mlx, const char *frc_name)
+t_frc	*launch_frc(mlx_t *mlx, const char *frc_name)
 {
 	t_frc	*frc;
 
 	frc = init_frc(mlx);
 	if (!frc)
-		return ;
+		return (NULL);
 	if (ft_strncmp(frc_name, "mandelbrot", 11) == 0)
 	{
 		frc->type = MANDELBROT;
@@ -33,28 +33,40 @@ void	launch_frc(mlx_t *mlx, const char *frc_name)
 	{
 		ft_printf("Error: Unknown frc type. Use 'mandelbrot' or 'julia'.\n");
 		free(frc);
-		return ;
+		return (NULL);
 	}
 	mlx_key_hook(mlx, &handle_key, frc);
+	return (frc);
+}
+
+void	free_frc(t_frc *frc)
+{
+	if (frc)
+	{
+		if (frc->img)
+			mlx_delete_image(frc->mlx, frc->img);
+		free(frc);
+	}
 }
 
 int	main(int argc, char **argv)
 {
 	mlx_t	*mlx;
+	t_frc	*frc;
 
 	if (argc != 2)
-	{
-		ft_printf("Usage: ./fractol [frc_type]\n");
-		return (1);
-	}
+		ft_error("Usage: ./fractol [fractal_type]\n");
 	mlx = mlx_init(WID, HEI, "fractol", 1);
 	if (!mlx)
+		ft_error("Error: Failed to initialize MLX.\n");
+	frc = launch_frc(mlx, argv[1]);
+	if (!frc)
 	{
-		ft_printf("Error: Failed to initialize MLX.\n");
+		mlx_terminate(mlx);
 		return (1);
 	}
-	launch_frc(mlx, argv[1]);
 	mlx_loop(mlx);
+	free_frc(frc);
 	mlx_terminate(mlx);
 	return (0);
 }
